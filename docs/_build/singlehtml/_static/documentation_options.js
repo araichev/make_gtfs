fc4baed29f5800322d41c81cd3a3673eecfb1bcc
Alex Raichev <araichev@mrcagney.com>
2026-04-07 12:13:46 +1200

    Fixed docs publishing

--- docs/_build/singlehtml/_static/documentation_options.js
+++ docs/_build/singlehtml/_static/documentation_options.js
@@ -1,9 +1,13 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '2.0.0',
-    LANGUAGE: 'None',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '4.2.0',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
+    BUILDER: 'singlehtml',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
